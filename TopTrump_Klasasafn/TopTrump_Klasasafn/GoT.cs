3e2b48102712238c241312e6db6a69918af1cc72
arnlaugur <laugi99@hotmail.com>
2017-04-27 09:34:30 +0000

    Baetting vid klassa

--- TopTrump_Klasasafn/TopTrump_Klasasafn/GoT.cs
+++ TopTrump_Klasasafn/TopTrump_Klasasafn/GoT.cs
@@ -8,6 +8,16 @@ namespace TopTrump_Klasasafn
 {
     class GoT
     {
-
+        private string Name { get; set; }
+        private string id { get; set; }
+        private string Age { get; set; }
+        private int Wit { get; set; }
+        private int Strength { get; set; }
+        private int Leadership { get; set; }
+        private int Piety { get; set; }
+        private int Prestige { get; set; }
+        private int Agility { get; set; }
+        private int Intrigue { get; set; }
+        private int Stewardship { get; set; }
     }
 }
