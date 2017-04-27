5f8c74f52d265dbc7046ce5c9124cbfed0b1fe61
BjarkiThor99 <Bjarki.thor@outlook.com>
2017-04-27 09:52:54 +0000

    Klasasafn(en í vinnslu)

--- TopTrump_Klasasafn/TopTrump_Klasasafn/LotR.cs
+++ TopTrump_Klasasafn/TopTrump_Klasasafn/LotR.cs
@@ -8,6 +8,14 @@ namespace TopTrump_Klasasafn
 {
     public class LotR
     {
-
+        private string Name { get; set; }
+        private int Id { get; set; }
+        private int Strength { get; set; }
+        private int Agility { get; set; }
+        private int Intellect { get; set; }
+        private int Stamina { get; set; }
+        private int Stealth { get; set; }
+        private int Courage { get; set; }
+        private int 
     }
 }
