cec5ca9d33a3cc2defee0224d1920648dbeea6cf
arnlaugur <laugi99@hotmail.com>
2017-05-02 11:03:22 +0000

    Vidbot vid klassasafn

--- TopTrump_Klasasafn/TopTrump_Klasasafn/GoT.cs
+++ TopTrump_Klasasafn/TopTrump_Klasasafn/GoT.cs
@@ -8,35 +8,37 @@ namespace TopTrump_Klasasafn
 {
     class GoT
     {
-        private string Name { get; set; }
-        private string id { get; set; }
-        private string Age { get; set; }
-        private string CardType { get; set; }
-        private int Wit { get; set; }
-        private int Strength { get; set; }
-        private int Leadership { get; set; }
-        private int Piety { get; set; }
-        private int Prestige { get; set; }
-        private int Agility { get; set; }
-        private int Intrigue { get; set; }
-        private int Stewardship { get; set; }
+        private string[] Name { get; set; }
+        private string[] id { get; set; }
+        private string[] Age { get; set; }
+        private string[] CardType { get; set; }
+        private int[] Wit { get; set; }
+        private int[] Strength { get; set; }
+        private int[] Leadership { get; set; }
+        private int[] Piety { get; set; }
+        private int[] Prestige { get; set; }
+        private int[] Agility { get; set; }
+        private int[] Intrigue { get; set; }
+        private int[] Stewardship { get; set; }
         public GoT(string[] CardDeck) //Settur eiginleikana á réttan stað
         {
             foreach (string cards in CardDeck)
             {
                 string[] card = cards.Split(':');
-                Name = card[1];
-                id = card[2];
-                Age = card[3];
-                CardType = card[4];
-                Wit = Convert.ToInt32(card[5]);
-                Strength = Convert.ToInt32(card[6]);
-                Leadership = Convert.ToInt32(card[7]);
-                Piety = Convert.ToInt32(card[8]);
-                Prestige = Convert.ToInt32(card[9]);
-                Agility = Convert.ToInt32(card[10]);
-                Intrigue = Convert.ToInt32(card[11]);
-                Stewardship = Convert.ToInt32(card[12]);
+                int i = 0;
+                Name[i] = card[1];
+                id[i] = card[2];
+                Age[i] = card[3];
+                CardType[i] = card[4];
+                Wit[i] = Convert.ToInt32(card[5]);
+                Strength[i] = Convert.ToInt32(card[6]);
+                Leadership[i] = Convert.ToInt32(card[7]);
+                Piety[i] = Convert.ToInt32(card[8]);
+                Prestige[i] = Convert.ToInt32(card[9]);
+                Agility[i] = Convert.ToInt32(card[10]);
+                Intrigue[i] = Convert.ToInt32(card[11]);
+                Stewardship[i] = Convert.ToInt32(card[12]);
+                i++;
             }
         }
         public string InitCards()
