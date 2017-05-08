c36793bf2b60040556897849390fa9b131fed073
arnlaugur <laugi99@hotmail.com>
2017-05-08 16:18:37 +0000

    Vinna meira

--- Lokaverkefni_TopTrumps/Lokaverkefni_TopTrumps/Program.cs
+++ Lokaverkefni_TopTrumps/Lokaverkefni_TopTrumps/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TopTrump_Klasasafn;
 
 namespace Lokaverkefni_TopTrumps
 {
@@ -10,6 +11,24 @@ namespace Lokaverkefni_TopTrumps
     {
         static void Main(string[] args)
         {
+            Class1 KlassLibrary = new Class1();
+            int UserInput = 99;
+            while(UserInput != 0)
+            {
+                Console.WriteLine("Veldu Hvað þú vilt gera");
+                Console.WriteLine("1. Sýna Reglur");
+                Console.WriteLine("2. Taka leik");
+                UserInput = Convert.ToInt32(Console.ReadLine());
+                switch(UserInput)
+                {
+                    case 1:
+                        Console.WriteLine(KlassLibrary.Reglur());
+                        Console.ReadKey();
+                        break;
+                }
+            }
+
+            
         }
     }
 }
--- TopTrump_Klasasafn/TopTrump_Klasasafn/Class1.cs
+++ TopTrump_Klasasafn/TopTrump_Klasasafn/Class1.cs
@@ -15,5 +15,12 @@ namespace TopTrump_Klasasafn
         {
             return "Í toptrumps er kortum skipt jafnt á milli leikmana og meiga þeir bara skoða efsta spilið sitt leikurinn byrjar á því að einn leikmaður velur grein til að keppa í og vinnur ef hann hefur hærri tölu í þeirri grein og tekur kortin sem voru notuð í þeirri umferð og draga spilarar efsta kort aftur og næsti leikmaður velur grein.";
         }
+
+        public int TakaLeik(int CardNum, int CompetitionCategory)
+        {
+            GoT got = new GoT();
+
+
+        }
     }
 }
